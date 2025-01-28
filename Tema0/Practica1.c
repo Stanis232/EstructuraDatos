07b279a0946345b028dbe656e3e1b5fb190c5dc1
stanis <st.yaruchyk@gmail.com>
2025-01-28 23:32:47 +0100

    primeros pasos en este entorno

---
+++ Tema0/Practica1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int num;
+    scanf("%d", &num);
+
+/**/    /*int * ptr = (int *) malloc (num * sizeof(int));*/
+    printf(ptr);
+
+}
+
